9fe51787ee4f647c84e83fb8831850c3fd71d6cd
Connor Bär <connor.baer@sumup.com>
2019-06-24 01:09:11 +0200

    feat: Export constants

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import notionBlocksToRichTextNodes from './notion-blocks-to-rich-text-nodes';
 import get from './utils/get';
 
+export { BLOCKS, MARKS, NOTION_BLOCKS, NOTION_MARKS } from './constants';
+
 export default function richTextFromNotion(page) {
   const blocks = get(page, 'recordMap.block', {});
   const content = notionBlocksToRichTextNodes(blocks);
